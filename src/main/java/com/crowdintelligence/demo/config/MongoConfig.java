29bc1c740e02b98c15af6f92611d262db8667dda
Hassan-Shakoor <hassanshakoor308@gmail.com>
2025-09-08 05:05:20 +1000

    initial dependencies

---
+++ src/main/java/com/crowdintelligence/demo/config/MongoConfig.java
@@ -0,0 +1,8 @@
+package com.crowdintelligence.demo.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MongoConfig implements WebMvcConfigurer {
+//    private final TenantInterceptor  tenantInterceptor;
+
+}
